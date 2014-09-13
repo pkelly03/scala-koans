c096a3ce5b8fcf397140cc1639ce5f0110a8035c
Paul Kelly <pkelly@equalexperts.com>
2014-09-13 06:36:36 +0100

    koans tidy up

--- src/test/scala/org/functionalkoans/forscala/AboutClasses.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutClasses.scala
@@ -11,16 +11,16 @@ class AboutClasses extends KoanSuite {
   class ClassWithValParameter(val name: String)
 
   koan("val parameters in class definition define getter") {
-    val aClass = new ClassWithValParameter("name goes here")
-    aClass.name should be(__)
+    val aClass = new ClassWithValParameter("Paul")
+    aClass.name should be("Paul")
   }
 
   koan("var parameters in class definition define getter and setter") {
     val aClass = new ClassWithVarParameter("description goes here")
-    aClass.description should be(__)
+    aClass.description should be("description goes here")
 
     aClass.description = "new description"
-    aClass.description should be(__)
+    aClass.description should be("new description")
   }
 
   // you can define class with private fields
@@ -28,8 +28,6 @@ class AboutClasses extends KoanSuite {
 
   koan("fields defined internally are private to class") {
     val aClass = new ClassWithPrivateFields("name")
-
-    // NOTE: aClass.name is not accessible
   }
 
 }
