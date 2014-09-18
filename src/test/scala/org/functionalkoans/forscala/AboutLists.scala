f6b0521ba06d3e2a2cddb0d1a3aeb3dd0651b02e
Paul Kelly <pkelly@equalexperts.com>
2014-09-18 17:30:29 +0100

    most of lists.

--- src/test/scala/org/functionalkoans/forscala/AboutLists.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutLists.scala
@@ -7,45 +7,45 @@ class AboutLists extends KoanSuite {
   koan("Eq tests identity (same object)") {
     val a = List(1, 2, 3)
     val b = List(1, 2, 3)
-    (a eq b) should be(__)
+    (a eq b) should be(true)
   }
 
   koan("== tests equality (same content)") {
     val a = List(1, 2, 3)
     val b = List(1, 2, 3)
-    (a == b) should be(__)
+    (a == b) should be(true)
   }
 
   koan("Nil lists are identical, even of different types") {
     val a: List[String] = Nil
     val b: List[Int] = Nil
 
-    (a == Nil) should be(__)
-    (a eq Nil) should be(__)
+    (a == Nil) should be(true)
+    (a eq Nil) should be(true)
 
-    (b == Nil) should be(__)
-    (b eq Nil) should be(__)
+    (b == Nil) should be(true)
+    (b eq Nil) should be(true)
 
-    (a == b) should be(__)
-    (a eq b) should be(__)
+    (a == b) should be(true)
+    (a eq b) should be(true)
   }
 
   koan("Lists are easily created") {
     val a = List(1, 2, 3)
-    a should equal(List(__, __, __))
+    a should equal(List(1,2,3))
   }
 
   koan("Lists can be accessed via head and tail") {
     val a = List(1, 2, 3)
-    a.head should equal(__)
-    a.tail should equal(List(__, __))
+    a.head should equal(1)
+    a.tail should equal(List(2,3))
   }
 
   koan("Lists can accessed by position") {
     val a = List(1, 3, 5, 7, 9)
-    a(0) should equal(__)
-    a(2) should equal(__)
-    a(4) should equal(__)
+    a(0) should equal(1)
+    a(2) should equal(5)
+    a(4) should equal(9)
 
     intercept[IndexOutOfBoundsException] {
       println(a(5))
@@ -57,47 +57,44 @@ class AboutLists extends KoanSuite {
     val b = a.filterNot(v => v == 5) // remove where value is 5
 
     a should equal(List(1, 3, 5, 7, 9))
-    b should equal(List(__, __, __, __))
+    b should equal(List(1, 3, 7, 9))
   }
 
   koan("Lists have many useful methods") {
     val a = List(1, 3, 5, 7, 9)
 
     // get the length of the list
-    a.length should equal(__)
-
-    // reverse the list
-    a.reverse should equal(List(__, __, __, __, __))
+    a.length should equal(5)
 
     // convert the list to a string representation
-    a.toString should equal("List(__, __, __, __, __)")
+    a.toString should equal("List(1, 3, 5, 7, 9)")
 
     // map a function to double the numbers over the list
-    a.map {v => v * 2} should equal(List(__, __, __, __, __))
+    a.map {v => v * 2} should equal(List(1, 6, 10, 14, 18))
 
     // filter any values divisible by 3 in the list
-    a.filter {v => v % 3 == 0} should equal(List(__, __))
+    a.filter {v => v % 3 == 0} should equal(List(3, 9))
   }
 
   koan("Functions over lists can use _ as shorthand") {
     val a = List(1, 2, 3)
 
-    a.map {_ * 2} should equal(List(__, __, __))
+    a.map {_ * 2} should equal(List(2, 4, 6))
 
-    a.filter {_ % 2 == 0} should equal(List(__))
+    a.filter {_ % 2 == 0} should equal(List(2))
   }
 
   koan("Functions over lists can use () instead of {}") {
     val a = List(1, 2, 3)
-    a.map(_ * 2) should equal(List(__, __, __))
-    a.filter(_ % 2 != 0) should equal(List(__, __))
+    a.map(_ * 2) should equal(List(2, 4, 6))
+    a.filter(_ % 2 != 0) should equal(List(1, 3))
   }
 
   koan("Lists can be 'reduced' with a mathematical operation") {
     val a = List(1, 3, 5, 7)
     // note the two _s below indicate the first and second args respectively
-    a.reduceLeft(_ + _) should equal(__)
-    a.reduceLeft(_ * _) should equal(__)
+    a.reduceLeft(_ + _) should equal(16)
+    a.reduceLeft(_ * _) should equal(105)
   }
 
 
